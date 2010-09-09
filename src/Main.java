030acfb77ce3c38b6ac5cc8a36525ee661b26a50
Tom Kucharski <kucharski.tom@gmail.com>
2010-09-09 16:10:16 +0200

    pierwsze klasy java w GIT :-)

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * @author tomekk
+ * @since 2010-09-09, 15:58:58
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/SecuredClass.java
@@ -0,0 +1,6 @@
+/**
+ * @author tomekk
+ * @since 2010-09-09, 15:58:47
+ */
+public class SecuredClass {
+}
